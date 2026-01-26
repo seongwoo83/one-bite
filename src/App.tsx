e2987a130b6473c95eb73c1e25061570308d9c0c
jsw <jeongseongwoo83@daum.net>
2026-01-26 23:34:01 +0900

    feat: Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
+import IndexPage from "@/pages/index-page";
+import SignInPage from "@/pages/sign-in-page";
+import SignUpPage from "@/pages/sign-up-page";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<IndexPage/>}/>
+      <Route path="/sign-in" element={<SignInPage/>}/>
+      <Route path="/sign-up" element={<SignUpPage/>}/>
+    </Routes>
+  );
 }
 
 export default App;
